b94d88182aa4084bd090c1d548f78b6be5d1c180
joonsub <joonsubtalk@gmail.com>
2017-02-03 22:06:16 -0800

    basic start

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+          <div className="panel panel-default post-editor">
+            <div className="panel-body">
+                Hello World!
+            </div>
+          </div>
+      </div>
+    );
+  }
+}
+
+export default App;
