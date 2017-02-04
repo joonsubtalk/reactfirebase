b4a80b841207a320031b4d89d5358b755ef8717b
joonsub <joonsubtalk@gmail.com>
2017-02-03 22:19:43 -0800

    Slight refactor by making separate Post component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
+import Post from './Post/component/Post';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div>
+          <Post/>
           <div className="panel panel-default post-editor">
             <div className="panel-body">
-                Hello World!
+                <textarea className="form-control post-editor-input" />
+                <button className="btn btn-success post-editor-button">Post</button>
             </div>
           </div>
       </div>
---
+++ src/Post/component/Post.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Post = (props) => (
+    <div className="panel panel-default post-body">
+        <div className="panel-body">
+            Hello I'm a post
+        </div>
+    </div>
+);
+
+export default Post;
